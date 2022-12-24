0b66e8aea271e79134aeb5b06ba60441e8bc2c6c
Neha Prajapati <neha@useblitz.com>
2022-12-24 10:50:55 +0530

    applied pagination and styling

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import '../styles/pagination.css';
 
-const Pagination = () => {
+const Pagination = ({page, productsData, setLoading, skip, setSkip}) => {
 
   return (
-
-    <div>
-
+    <div className="pagination">
+<button className="btn btn-secondary prev" disabled={skip === 1} onClick={()=>{
+   setSkip(skip-1)
+   setLoading(true)
+ }}>Prev</button>
+  <h5 >Page:{skip}</h5>
+ <button disabled={page > productsData.length} className="btn btn-secondary"  onClick={()=>{
+   setSkip(skip+1)
+   setLoading(true)
+ }}>Next</button>
     </div>
   )
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Axios from 'axios';
 import ProductCard from "./ProductCard";
+import Pagination from "./Pagination";
 
 const Products = () => {
   const [productsData, setProductsData] = useState([]);
+  const [page, setPage] = useState(12);
+  const [skip, setSkip] = useState(1);
+  const [loading,setLoading]= useState(false);
+  console.log(skip,"skip");
 
   async function getSubscriptionInfo() {
-    const url = 'https://dummyjson.com/products';
+    const url = `https://dummyjson.com/products?limit=${page}&skip=${skip}`;
     try {
       const { data } = await Axios.get(url);
       console.log("data-------",data);
       setProductsData(data.products);
+      setLoading(false);
       return data;
     } catch (e) {
       console.error('getSubscriptionInfo error\n', e);
@@ -18,15 +24,19 @@ const Products = () => {
   };
 
   useEffect(() => {
-    getSubscriptionInfo();
-  }, [])
+    getSubscriptionInfo(page)
+},[page, skip])
   
 
   return (
+    <div className="products-main">
+      {loading && <div>loading...</div>}
     <div className="products-cont">
    {productsData && productsData.map((item) => (
     <ProductCard item={item} />
    ))}
+  </div>
+   <Pagination page={page} productsData={productsData} setLoading={setLoading} skip={skip} setSkip={setSkip}/>
   </div>
   )
 };
