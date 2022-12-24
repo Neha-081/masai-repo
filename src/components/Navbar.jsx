5409b19d30adc8a34f20f7df3ba740eea8ec9867
Neha Prajapati <neha@useblitz.com>
2022-12-24 09:34:53 +0530

    react setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/navbar.css';
+
+const Navbar = () => {
+
+  return (
+
+    <div className="navbar">
+   <h1>Single Page E-Commerce App</h1>
+    </div>
+  )
+};
+
+export default Navbar;
