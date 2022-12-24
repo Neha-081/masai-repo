e832308c58070a2160b9dcd20d409f2a118873bd
Neha Prajapati <neha@useblitz.com>
2022-12-24 09:59:01 +0530

    fetch data from products api and rendered on products page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import '../styles/products.css'
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
 
   return (
-
-    <div>
-
+<div className="products">
+      <img src={item.thumbnail}/>
+      <h3>{item.title}</h3>
+      <h3>{item.description}</h3>
+      <h3>Brand - {item.brand}</h3>
+      <h3>Price - {item.price}</h3>
     </div>
   )
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from 'axios';
+import ProductCard from "./ProductCard";
 
 const Products = () => {
+  const [productsData, setProductsData] = useState([]);
+
+  async function getSubscriptionInfo() {
+    const url = 'https://dummyjson.com/products';
+    try {
+      const { data } = await Axios.get(url);
+      console.log("data-------",data);
+      setProductsData(data.products);
+      return data;
+    } catch (e) {
+      console.error('getSubscriptionInfo error\n', e);
+    }
+  };
+
+  useEffect(() => {
+    getSubscriptionInfo();
+  }, [])
+  
 
   return (
-
-    <div>
-
-    </div>
+    <div className="products-cont">
+   {productsData && productsData.map((item) => (
+    <ProductCard item={item} />
+   ))}
+  </div>
   )
 };
 
